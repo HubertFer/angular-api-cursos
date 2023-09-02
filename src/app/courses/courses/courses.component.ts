62f4073ba58ca499b6f485ea9d38cd335e9adbea
HubertFer <humbertofernandezsosa@gmail.com>
2023-09-02 13:04:03 -0300

    first commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+
+import { Course } from '../models/course';
+import { CoursesService } from '../services/courses.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+
+export class CoursesComponent implements OnInit{
+
+  courses$:  Observable<Course[]>;
+
+  displayedColumns = ['name', 'category'];
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
+
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar dados')
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+}
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Course } from '../models/course';
+import { delay, tap } from 'rxjs/operators';
+import { first } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private readonly API = '/assets/courses.json';
+
+  list()  {
+    return  this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(2000),
+      tap(courses => console.log(courses))
+    );
+  }
+}
